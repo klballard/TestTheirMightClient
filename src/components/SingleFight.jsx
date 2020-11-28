e62abd98bd46ed47391c159bd9239b77f9cfcd60
klballard <kyleleeballard@gmail.com>
2020-11-28 13:05:17 -0500

    1v1 functionality is mostly completed

--- src/components/SingleFight.jsx
+++ src/components/SingleFight.jsx
@@ -2,13 +2,15 @@ import React, {useState, useEffect} from 'react';
 import {Card, Col, Row, Button} from 'reactstrap';
 
 const SingleFight = (props) => {
-    const [fighter, setFighter] = useState([]);
+
+    const [fighterOne, setFighterOne] = useState([]);
+    const [fighterTwo, setFighterTwo] = useState([]);
     const [fighters, setFighters] = useState([]);
     const [selectValue, setSelectValue] = useState([]);
     const [secondValue, setSecondValue] = useState([]);
 
-    const fetchFighter = (fighter) => {
-        fetch(`https://testtheirmightheroku.herokuapp.com/fighter/${fighter.id}`, {
+    const fetchFighterOne = () => {
+        fetch(`https://testtheirmightheroku.herokuapp.com/fighter/${selectValue}`, {
             method: 'GET',
             headers: new Headers({
                 'Content-Type': 'application/json',
@@ -16,7 +18,20 @@ const SingleFight = (props) => {
             })
         }).then((res) => res.json())
         .then((listData) => {
-            setFighter(listData);
+            setFighterOne(listData);
+        })
+    }
+
+    const fetchFighterTwo = () => {
+        fetch(`https://testtheirmightheroku.herokuapp.com/fighter/${secondValue}`, {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.token
+            })
+        }).then((res) => res.json())
+        .then((listData) => {
+            setFighterTwo(listData);
         })
     }
 
@@ -44,54 +59,85 @@ const SingleFight = (props) => {
         })
     }
     */
+   function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+    }
+
+
+    const fightResults = () => {
+        if(sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat]) > sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])){
+            alert(`\n                    ${fighterOne.fighterName}'s power level: ${sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat])}
+                    ${fighterTwo.fighterName}'s power level: ${sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])}\n
+                    ${fighterOne.fighterName} wins!`)
+        }
+        else if(sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat]) < sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])){
+            alert(`\n                    ${fighterOne.fighterName}'s power level: ${sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat])}
+                    ${fighterTwo.fighterName}'s power level: ${sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])}\n
+                    ${fighterTwo.fighterName} wins!`)
+        }
+        else if(sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat]) === sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])){
+            alert(`\n                    ${fighterOne.fighterName}'s power level: ${sum([fighterOne.intelligence, fighterOne.strength, fighterOne.speed, fighterOne.durability, fighterOne.power, fighterOne.combat])}
+                    ${fighterTwo.fighterName}'s power level: ${sum([fighterTwo.intelligence, fighterTwo.strength, fighterTwo.speed, fighterTwo.durability, fighterTwo.power, fighterTwo.combat])}\n
+                     It's a draw! Good fight.`)
+        }
+    }
 
     return(
         <div>
+            <div style={{ marginTop:'140px', textAlign:'center'}}><h4>Pit two fighters against each other and see who wins!</h4></div>
+            <div>
             <Row>
             <Col>
-            <Card style={{ marginTop:'200px'}}>
-            <Col>
-            <select value={selectValue} onChange={e => setSelectValue(e.currentTarget.value)}>{fighters.map(fighter => (
-                <option key={fighter.id} value={fighter.id}>{fighter.fighterName}</option>
-            ))}</select>
-            </Col>
-            <Col>
-            <Button /*onClick={fetchFighterOne}*/>Load Fighter One</Button>
-            </Col>
-            <Col>
-            <h3>test</h3>
-            {fighters.map(fighter => (
-                <tr key={fighter.id}>
-                <th><img src={fighter.image} width='50' height='50'></img></th>
-                <td>{fighter.name}</td>
-                <td>{fighter.intelligence}</td>
-                <td>{fighter.strength}</td>
-                <td>{fighter.speed}</td>
-                <td>{fighter.durability}</td>
-                <td>{fighter.power}</td>
-                <td>{fighter.combat}</td>
-                </tr>
-            ))}
+            <Card style={{ textAlign:'center', height:'500px', width:'300px', marginTop:'140px', marginLeft:'auto', marginRight:'120px'}}>
+                <Col>
+                    <table key={fighterOne.id}>
+                    <tr><img alt='Select first fighter..' src={fighterOne.image} width='100%' height='300px'></img></tr>
+                    </table>
+                </Col>
+                <Col>
+                    <select value={selectValue}  onChange={e => setSelectValue(e.currentTarget.value)}>{fighters.map(fighter => (
+                    <option key={fighter.id} value={fighter.id}>{fighter.fighterName}</option>
+                    ))}</select>
+                <br/>
+            <Button onClick={fetchFighterOne}>Ready Player One?</Button>
             </Col>
             </Card>
             </Col>
-
+            <div style={{marginTop:'400px'}}>
+            <h3>VS.</h3>
+            <br/>
+            <Button onClick={fightResults}>FIGHT!</Button>
+            </div>
             <Col>
-            <Card style={{ marginTop:'200px'}}>
-            <Col>
-            <select value={secondValue} onChange={e => setSecondValue(e.currentTarget.value)}>{fighters.map(fighter => (
-                <option key={fighter.id} value={fighter.id}>{fighter.fighterName}</option>
-            ))}</select>
-            </Col>
-            <Col>
-            <Button /*onClick={fetchFighterTwo}*/>Load Fighter Two</Button>
-            </Col>
-            <Col>
-            
-            </Col>
+            <Card style={{ textAlign:'center', height:'500px', width:'300px', marginTop:'140px', marginLeft:'120px', marginRight:'auto'}}>
+                <Col>
+                <table key={fighterTwo.id}>
+                <tr><img alt='Select second fighter..' src={fighterTwo.image} width='100%' height='300px'></img></tr>
+                </table>
+                </Col>
+                <Col>
+                    <select value={secondValue} onChange={e => setSecondValue(e.currentTarget.value)}>{fighters.map(fighter => (
+                        <option key={fighter.id} value={fighter.id}>{fighter.fighterName}</option>
+                    ))}</select>
+                    <br/>
+                <Button onClick={fetchFighterTwo}>Ready Player Two?</Button>
+                </Col>
             </Card>
             </Col>
             </Row>
+            </div>
         </div>
     )
 }
