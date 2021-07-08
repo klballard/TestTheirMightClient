a7c93f98dd3612e67a69a5c6e76afc6175ba102b
klballard <kyleleeballard@gmail.com>
2021-07-08 18:38:54 -0400

    editing environment helper to be http instead of https

--- src/helpers/environment.jsx
+++ src/helpers/environment.jsx
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
-        APIURL= 'https://testtheirmightserver.herokuapp.com';
+        APIURL= 'http://testtheirmightserver.herokuapp.com';
         break;
     case 'testtheirmightclient.herokuapp.com':
-        APIURL = 'https://testtheirmightserver.herokuapp.com'
+        APIURL = 'http://testtheirmightserver.herokuapp.com'
 }
 
 export default APIURL;
