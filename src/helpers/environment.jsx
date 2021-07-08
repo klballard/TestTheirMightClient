2e53e57ae21f1a0e1d213b130cfdcb6da67d8e4f
klballard <kyleleeballard@gmail.com>
2021-07-08 19:15:58 -0400

    removed cors proxy from apiurl in env helpers

--- src/helpers/environment.jsx
+++ src/helpers/environment.jsx
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
-        APIURL= 'https://hidden-basin-37125.herokuapp.com/https://testtheirmightserver.herokuapp.com';
+        APIURL= 'https://testtheirmightserver.herokuapp.com';
         break;
     case 'testtheirmightclient.herokuapp.com':
-        APIURL = 'https://hidden-basin-37125.herokuapp.com/https://testtheirmightserver.herokuapp.com'
+        APIURL = 'https://testtheirmightserver.herokuapp.com'
 }
 
 export default APIURL;
